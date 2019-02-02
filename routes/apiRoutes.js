ec072ad8a56d216aa31faa5285a456c6827eedfa
Zach Merel <zachmerel@gmail.com>
2019-02-02 14:28:47 -0600

    created database and jaws connection

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,23 +1,25 @@
 var db = require("../models");
 
-module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
-    });
-  });
+module.exports = app => {
+  // // Get all examples
+  // app.get("/api/examples", (req, res) => {
+  //   db.Flickscore.findAll({}).then(dbFlickscore => {
+  //     res.json(dbFlickscore);
+  //   });
+  // });
 
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
+  // Create a new movie title & movie score.
+  app.post("/api/flickscores/", (req, res) => {
+    db.Flickscore.create(req.body).then(dbFlickscore => {
+      res.json(dbFlickscore);
     });
   });
 
   // Delete an example by id
   app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
+    db.Example.destroy({ where: { id: req.params.id } }).then(function(
+      dbExample
+    ) {
       res.json(dbExample);
     });
   });
