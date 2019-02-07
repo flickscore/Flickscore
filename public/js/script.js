199fed993c2e7c87efa43f069bd256f0e3b1d48e
maurera <42736637+maurera91@users.noreply.github.com>
2019-02-06 19:16:34 -0600

    message

--- public/js/script.js
+++ public/js/script.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
   $(".likeButton").on("click", function(event) {
     let id = $(this).data("id");
 
-    let originalScore = $.get(`/api/Flickscore/${id}`, (res, status) => {
+    let originalScore = $.get(`/api/movies/${id}`, (res, status) => {
       console.log(`RESULT: ${res}`);
       console.log(`STATUS: ${status}`);
       return res.score;
