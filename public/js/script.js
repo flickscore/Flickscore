78b17f3c37cce91ba75c3ef3a449ab493a6753ff
maurera <42736637+maurera91@users.noreply.github.com>
2019-02-09 12:47:06 -0600

    made a top ten list of movies with a modal

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,4 @@
-let movieSort = require("./movieSort");
-console.log(movieSort);
 $(document).ready(function () {
-  let $movieContainer = $(".movieContainer");
   // eslint-disable-next-line no-unused-vars
   $("#likeButton").on("click", function (event) {
     let title = $(this).attr("title");
@@ -22,24 +19,32 @@ $(document).ready(function () {
 
   getMovies();
 
-  function initializeData() {
-    $movieContainer.empty();
-    let dataToAdd = [];
-    for (i = 0; i < movies.length; i++) {
-      dataToAdd.push(createNewMovie(movies[i]));
-    }
-    $movieContainer.prepend(dataToAdd);
-  }
+  // function initializeData() {
+  //   $movieContainer.empty();
+  //   let dataToAdd = [];
+  //   for (i = 0; i < movies.length; i++) {
+  //     dataToAdd.push(createNewMovie(movies[i]));
+  //   }
+  //   $movieContainer.prepend(dataToAdd);
+  // }
 
   function getMovies() {
     $.get("api/movies", data => {
       movies = data;
-      initializeData();
+      console.log(movies);
+      // initializeData();
     });
   }
-  $("topTenBtn").on("click", function () {
+  $("#topTenBtn").on("click", function () {
+    $("#tenList").empty();
     getMovies();
-
-
+    let sortedMovies = [];
+    sortedMovies = divide(movies).reverse();
+    console.log(sortedMovies);
+    for (let i = 0; i < 10; i++) {
+      // eslint-disable-next-line quotes
+      // eslint-disable-next-line prettier/prettier
+      $("#tenList").append(`<li>${sortedMovies[i].movieTitle}: ${sortedMovies[i].movieScore} Likes</li>`);
+    }
   });
 });
